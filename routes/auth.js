259a7943cb4990813dabc9626c54556c49d07bd8
Astroth1984 <elmounir.othmane666@gmail.com>
2021-11-19 16:44:43 +0100

    NodeJs JWT Implementation

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/user");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async(req, res) => {
@@ -41,11 +42,20 @@ router.post("/login", async(req, res) => {
         OriginalPassword !== req.body.password &&
             res.status(401).json("Wrong Crendentials!");
 
+        const accessToken = jwt.sign({
+                id: user._id,
+                isAdmin: user.isAdmin,
+
+            },
+            process.env.JWT_SEC, { expiresIn: "3d" }
+        );
+
+
         //Send all the info but Password to User
         //MongoDB store our users in a document under _doc
         const { password, ...others } = user._doc;
         //Return the user infos-{password} if the login is Successfull
-        res.status(200).json(others);
+        res.status(200).json({...others, accessToken });
         // res.status(200).json(user);
     } catch (err) {
         res.status(500).json(err);
