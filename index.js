4593d701af04152833eb8eda1203b48f7f60ff70
Astroth1984 <elmounir.othmane666@gmail.com>
2021-11-20 01:51:59 +0100

    Add Products Route

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
-const authRoute = require("./routes/auth")
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 
 dotenv.config();
 
@@ -15,8 +16,9 @@ mongoose
     });
 
 app.use(express.json());
-app.use("/api/auth", authRoute)
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server is Running!");
