cf1ee358ba0175b29f1879253dd416fd5d0cf419
Astroth1984 <elmounir.othmane666@gmail.com>
2021-11-20 02:32:49 +0100

    NodeJs Order CRUD Operations with JWTé

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server is Running!");
