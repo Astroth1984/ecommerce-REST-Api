eb1142145bd9bbbc7ba413e3ca386fc42444d66e
Astroth1984 <elmounir.othmane666@gmail.com>
2021-11-20 02:29:27 +0100

    NodeJs Cart CRUD Operations & JWT

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server is Running!");
